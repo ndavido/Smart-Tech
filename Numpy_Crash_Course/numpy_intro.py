f51f08c8cca8993a70623317b11132bee06e8816
ndavido <D00239107@student.dkit.ie>
2023-09-28 13:22:52 +0100

    :sparkles: Numpy usage

--- Numpy_Crash_Course/numpy_intro.py
+++ Numpy_Crash_Course/numpy_intro.py
@@ -2,8 +2,98 @@
 import numpy as np
 
 def main():
-    add_two_vectors_using_lists()
-    add_two_vectors_using_numpy()
+    #! add_two_vectors_using_lists()
+    #! add_two_vectors_using_numpy()
+    #! test_multi_dim_arrays()
+    #! index_multi_dim_array()
+    #! one_dim_slicing()
+    #! reshaping_array()
+    #! test_conditionals()
+    #! array_flattening()
+    #! test_transpose()
+    #! matrix_multiplication()
+    #! test_stacking()
+    test_depth_stacking()
+    
+def test_depth_stacking():
+    x = np.arange(4).reshape(2,2)
+    y = x ** 2
+    z = np.dstack((x,y))
+    print(z)
+    print(z.shape)
+    
+def test_stacking():
+    x = np.arange(4).reshape(2,2)
+    y = np.arange(4,8).reshape(2,2)
+    z = np.hstack((x,y))
+    w = np.column_stack((x,y))
+    v = np.concatenate((x,y), axis=1)
+    
+    h = np.vstack((x,y))
+    i = np.row_stack((x,y))
+    j = np.concatenate((x,y), axis=0)
+    print(h)
+    
+    print(z==w)
+    print(z==v)
+    
+def matrix_multiplication():
+    mat_a = np.matrix([0,3,5,5,5,2]).reshape(2,3)
+    mat_b = np.matrix([3,4,3,-2,4,-2]).reshape(3,2)
+    print(mat_a)
+    print(mat_a * mat_b)
+    
+def test_transpose():
+    x = np.arange(9).reshape(3,3)
+    print(x)
+    print(x.transpose())
+    print(np.zeros((3,2)))
+    print(np.ones((3,2)))
+    print(np.eye(3))
+    print(np.random.rand(4,4))
+    
+def array_flattening():
+    x = np.arange(9).reshape(3,3)
+    print(x)
+    flattened_array = x.flatten()
+    flattened_array[0] = 1000000
+    print(flattened_array)
+    
+def test_conditionals():
+    x = np.arange(10)    
+    print(x.min())
+    
+def reshaping_array():
+    x = np.arange(18).reshape(3,3,2)
+    print(x[1])    
+
+def one_dim_slicing():
+    x = np.arange(1, 10)
+    print(x[:7])
+ 
+def index_multi_dim_array():
+    x = np.arange(3)
+    y = np.arange(3, 6)
+    z = np.arange(255, 258)
+    multi_dim_array = np.array([x,y,z], dtype=np.int8)
+    print(multi_dim_array)
+    print(multi_dim_array.dtype)
+    
+def test_multi_dim_arrays():
+    x = np.arange(3)
+    y = np.arange(3)
+    z = np.arange(3)
+    multi_dim_array = np.array([x,y,z])
+    print(multi_dim_array)
+    print(multi_dim_array.shape)
+    w = np.linspace(1,10,50)
+    print(w)
+    b = np.arange(1,30,3)
+    print(b)
+    bl = np.linspace(1,30,3)
+    print(bl)
+    bl = np.linspace(1,30,3, endpoint=False)
+    print(bl)
     
 def add_two_vectors_using_lists():
     list_two = list(range(1,4))
