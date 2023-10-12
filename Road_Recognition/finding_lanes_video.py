83c5f7f03bf4802fe9128df0a1eee3212ea94aad
ndavido <D00239107@student.dkit.ie>
2023-10-12 11:21:19 +0100

    :sparkles: Fixed Ouput Issue

--- Road_Recognition/finding_lanes_video.py
+++ Road_Recognition/finding_lanes_video.py
@@ -6,25 +6,24 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    # image = cv2.imread('road.jpg')
-    cap = cv2.VideoCapture('JohnM50.mp4')
+    # image = cv2.imread("road.jpg")
+    cap = cv2.VideoCapture("JohnM50.mp4")
     while (cap.isOpened()):
-        _, frame = cap.read()
-        if frame is not None:
-            print(frame.shape)
-            frame = cv2.resize(frame, (960, 1280))
-            lane_image = np.copy(frame)
-            canny_image = find_edges(lane_image)
-            cropped_image = region_of_interest(canny_image)
-            lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]),
-                                    minLineLength=40, maxLineGap=100)
-            averaged_lines = average_slope_intercept(lane_image, lines)
-            line_image = display_lines(lane_image, averaged_lines)
-            combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-            cv2.imshow("Combo image", combo_image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
+        ret, frame = cap.read()  # Store the return value in ret
+        if not ret or frame is None:
+            break  # Exit the loop if there are no more frames
+
+        frame = cv2.resize(frame, (960, 1280))
+        lane_image = np.copy(frame)
+        canny_image = canny(lane_image)
+        cropped_image = region_of_interest(canny_image)
+        lines = cv2.HoughLinesP(cropped_image, 1, np.pi/180, 100, np.array([]),
+                                minLineLength=40, maxLineGap=5)
+        averaged_lines = average_slope_intercept(lane_image, lines)
+        line_image = display_lines(lane_image, averaged_lines)
+        combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+        cv2.imshow("Combo image", combo_image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
@@ -33,14 +32,14 @@ def main():
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
-    if lines is None:
-        return None
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
-        if slope < 0:
+        if abs(slope) < 0.001:
+            continue
+        elif slope < 0:
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
@@ -52,16 +51,12 @@ def average_slope_intercept(image, lines):
 
 
 def make_coordinates(image, line_parameters):
-    print(line_parameters)
-    if line_parameters is None:
-        return None
     slope, intercept = line_parameters
-    if line_parameters is not None and abs(slope) > 0.001:
-        y1 = image.shape[1]
-        y2 = int((y1 * 9/20))
-        x1 = int((y1 - intercept)/slope)
-        x2 = int((y2 - intercept)/slope)
-        return np.array([x1, y1, x2, y2])
+    y1 = image.shape[0]
+    y2 = int(y1 * (9/20))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
 
 
 def display_lines(image, lines):
@@ -69,7 +64,7 @@ def display_lines(image, lines):
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (0, 128, 0), 2)  # bgr
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
     return line_image
 
 
@@ -84,10 +79,10 @@ def region_of_interest(image):
     return masked_image
 
 
-def find_edges(image):
+def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny_image = cv2.Canny(blur, 50, 150)
+    canny_image = cv2.Canny(blur, 50, 100)
     return canny_image
 
 
