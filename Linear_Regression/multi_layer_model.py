991f43aed632ce87ec55d7694bc25b444d883cc8
ndavido <D00239107@student.dkit.ie>
2023-10-19 13:01:48 +0100

    :sparkles: Begining of multi layer model

--- Linear_Regression/multi_layer_model.py
+++ Linear_Regression/multi_layer_model.py
@@ -3,6 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
 np.random.seed(0)
 n_pts = 500
@@ -12,4 +15,16 @@ X, y = datasets.make_circles(
 
 plt.scatter(X[y == 0, 0], X[y == 0, 1])
 plt.scatter(X[y == 1, 0], X[y == 1, 1])
+plt.show()
+
+model = Sequential()
+model.add(Dense(3, input_shape=(2,), activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(Adam(learning_rate=0.01),
+              'binary_crossentropy', metrics=['accuracy'])
+h = model.fit(x=X, y=y, verbose=1, batch_size=20, epochs=100, shuffle='true')
+plt.plot(h.history['accuracy'])
+plt.xlabel('epoch')
+plt.legend(['accuracy'])
+plt.title('accuracy')
 plt.show()
